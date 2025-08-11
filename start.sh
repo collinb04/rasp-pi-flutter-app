abe7525f3ebce75dce3731aa7e9588fbd936c464
Collin Brennan <brenncol@mail.gvsu.edu>
2025-08-11 05:28:03 -0900

    fix file discovery

--- start.sh
+++ start.sh
@@ -28,12 +28,12 @@ done
 echo "[+] Serving Flutter web app..."
 
 cd "$PROJECT_DIR/frontend/build/web" || exit 1
-nohup python3 -m http.server 8001 > ../../http_server.log 2>&1 &
+nohup python3 -m http.server 8080 > ../../http_server.log 2>&1 &
 
 sleep 2
 
 echo "[+] Starting Chromium in kiosk mode..."
-DISPLAY=:0 exec chromium-browser --kiosk http://localhost:8001 --noerrdialogs --disable-infobars --no-sandbox
+DISPLAY=:0 exec chromium-browser --kiosk http://localhost:8080 --noerrdialogs --disable-infobars --no-sandbox --disable-cache
 
 echo "[✓] App running in kiosk mode"
 
